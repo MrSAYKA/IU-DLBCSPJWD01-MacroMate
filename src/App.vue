ffb4b51a18b32a3bf636bea6c863e00a6892d26e
Abdelrahman Galal <imsvgxxegyxx@gmail.com>
2026-04-27 18:05:13 +0200

    Initial comment / Vue.js and Front End setup

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const count = ref(0)
+const name = ref('Abdelrahman')
+const age = ref(22)
+const year = ref(2026)
+const emoji = ref('🤩')
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>
+  <header>
+  <button @click="increment">Count is: {{ count }}</button>
+    <span> {{emoji}} </span>
+    <div>
+      <h1>Hello my name is {{ name }}!</h1>
+    </div>
+  </header>
+  <footer>
+    <p>&copy; 2026</p>
+  </footer>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+button{
+  font-size: 1rem;
+  text-align: center;
+}
+header span{
+  font-size: 5rem;
+  text-align: center;
+}
+header div{
+  text-align: center;
+  font-size: 2rem;
+  color: darkcyan;
+  font-style: italic;
+}
+button {
+  font-weight: bold;
+}
+footer p {
+  text-align: center;
+  color: lavender;
+}
+</style>
